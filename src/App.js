522f8bd81fd65543ac30b68529e5fd5b2ec10677
Dorival Neto <dorivaltn@gmail.com>
2021-06-23 23:31:24 -0400

    Introducing Charka and a sample page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Heading,
+  Flex,
+  Text,
+  Box
+} from "@chakra-ui/react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex color="white" h="100%">
+
+      <Box w="70px" bg="gray.700">
+        <Text>Tool</Text>
+      </Box>
+
+      <Box w="250px" bg="gray.800">
+        <Text>Box 2</Text>
+      </Box>
+
+      <Box flex="1" bg="gray.900" padding="24px">
+        <Heading>Hello FlameCast!</Heading>
+        <FormControl id="email">
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+          <FormHelperText>We'll never share your email.</FormHelperText>
+        </FormControl>
+      </Box>
+    </Flex>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
