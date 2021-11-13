e9117936d3d978fadfb4c4e97f255bf5ccdc15e0
Shaun <smphayward@gmail.com>
2021-11-13 11:59:14 -0500

    #8 Detects if we're running in electron or not and loads a different storage provider based on this.

---
+++ src/app/shared/services/storage-providers/storage-provider-factory.ts
@@ -0,0 +1,17 @@
+import { ElectronFileSystemStorageProviderService } from "./electron-file-system-storage-provider.service";
+import { MockStorageProviderService } from "./mock-storage-provider.service";
+
+const storageProviderFactory = () => {
+  console.log("Determining storage provider from user agent.", navigator.userAgent);
+  if(navigator.userAgent.toLowerCase().indexOf("electron") > -1) {
+    console.log("Detected electron.");
+    return new ElectronFileSystemStorageProviderService();
+  }
+  else
+  {
+    console.log("Did not detect electron.");
+    return new MockStorageProviderService();
+  }
+}
+
+export default storageProviderFactory;
