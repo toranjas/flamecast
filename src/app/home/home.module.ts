b2e3d3a68409cd9e23a23e254993166ef9741667
Dorival Neto <dorivaltn@gmail.com>
2021-11-13 17:53:02 -0500

    Adding PrimeNg #2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { HomeRoutingModule } from './home-routing.module';
-
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
-// import { FullPageComponent } from '../layout/full-page/full-page.component';
 import { FullPageModule } from '../layout/full-page/full-page.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, FullPageModule]
+  imports: [
+    CommonModule,
+    SharedModule,
+    HomeRoutingModule,
+    FullPageModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+  ],
 })
 export class HomeModule {}
