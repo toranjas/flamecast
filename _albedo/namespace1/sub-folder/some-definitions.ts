ded19664635ed6ddc698fa83f6212a0038b3d970
Dorival Neto <dorivaltn@gmail.com>
2022-03-10 21:14:33 -0500

    Adding example for Albedo Lib

---
+++ _albedo/namespace1/sub-folder/some-definitions.ts
@@ -0,0 +1,6 @@
+export interface Dog {
+  name: string;
+  tag: string;
+  breed: string;
+  dob: Date;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Dog,
+  ExampleUtility,
+  VERY_IMPORTANT_INFO,
+} from '@toranjas/albedo/namespace1';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
   ngOnInit(): void {
-    console.log('HomeComponent INIT');
-  }
+    const util = new ExampleUtility();
 
+    console.log('HomeComponent INIT', util.getHello());
+
+    const spike: Dog = {
+      name: 'Spike',
+      tag: 'ES42332',
+      breed: 'Labrador',
+      dob: new Date(2012, 5, 23),
+    };
+
+    console.log(spike, VERY_IMPORTANT_INFO);
+  }
 }
