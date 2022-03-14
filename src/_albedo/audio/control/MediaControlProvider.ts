ea25e3d32b3d828171ef59ea4a1aab44bd7b4180
Shaun <smphayward@gmail.com>
2022-03-13 20:44:20 -0400

    #40 created MediaControlProvider to handle the audio graph.

---
+++ src/_albedo/audio/control/MediaControlProvider.ts
@@ -0,0 +1,33 @@
+export interface MeteringData {
+  leftCurrentPeakDBFS: number,
+  rightCurrentPeakDBFS: number,
+
+  leftHoldPeakDBFS: number,
+  rightHoldPeakDBFS: number,
+
+  leftIsClipping: boolean,
+  rightIsClipping: boolean
+}
+
+export interface MeteringCallback {
+  (meteringData: MeteringData): void;
+}
+
+export default interface MediaControlProvider {
+
+  // Input
+  audioInputId: string | null;
+  audioInputGain: number;
+  audioInputIsMuted: boolean;
+  inputMeteringCallback: MeteringCallback | null;
+
+  // Output
+  audioOutputId: string | null;
+
+  // Gain & Mute
+  
+
+  // Setup
+  setupAudioGraph: () => Promise<void>;
+
+}
