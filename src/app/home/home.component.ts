cfa29dc8ca1c8514afae0d2c1cf50ab975d0168b
Shaun Hayward <smphayward@gmail.com>
2022-03-18 14:10:13 -0400

    #31 added very bad mock UI for episode creation. Results in new episode creation. Tested on Linux only. (#46)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { createEpisodeAction } from '@app/episode/store/episode.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+
+
+  episodeIdentifier: string = '';
+
+  // Initialization
+  constructor(private store: Store) {}
+
   ngOnInit(): void {
     console.log('HomeComponent INIT');
   }
+
+  // Event handlers
+  episodeIdentifierChange = ($event: any) => {
+    this.episodeIdentifier = $event.target?.value ?? '';
+  };
+    
+  createEpisode = async () => {
+    await this.store.dispatch(
+      createEpisodeAction({ episodeLocation: this.episodeIdentifier }),
+    );
+  };
 }
